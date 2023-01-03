e5b525b92dcc88502733095dc630c24a1b47fd52
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-02 21:30:46 -0800

    Navbar updated with logos imported from utils

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { GoogleLogin, GoogleLogout } from '@react-oauth/google';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
+import Logo from '../utils/tiktik-logo.png';
+import Logo2 from '../utils/Logo2.png';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+      <Link href='/'>
+        <div className='w-[70px] md:w-[80px] p-1'>
+          <Image 
+            className='cursor-pointer'
+            src={Logo2}
+            alt='logo'
+            layout='responsive'
+          />
+          <Image 
+            className='cursor-pointer'
+            src={Logo}
+            alt='logo'
+            layout='responsive'
+          />
+        </div>
+      </Link>
+    </div>
   )
 }
 
