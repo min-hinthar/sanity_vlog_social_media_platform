25daeecd21a5df77145460504cda12d6fc62e9d5
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-03 12:02:03 -0800

    pages/api/post/index.ts created async await client.fetch allPostsQuery videos from sanity backend using token stored in .env.development

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
 import type { NextPage } from 'next';
+import axios from 'axios';
 
-const Home: NextPage = () => {
+// interface IProps {
+//   videos: 
+// }
+
+
+const Home: NextPage = ({ videos }) => {
+  console.log(videos)
+
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
   )
 }
+
+export const getServerSideProps = async () => {
+  const {data} = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data
+    }
+  }
+}
+
 
 export default Home
