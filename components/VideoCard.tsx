d86267402e42e0c3bb176f646f8cab60d25844ff
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-03 12:39:43 -0800

    next.config.js updated with images domain for user images url, VidoeCard updated with Username and Verified Blue Tick

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,16 +1,57 @@
-import React from 'react'
-import { Video } from '../types'
-import { NextPage } from 'next'
+import React, { useState, useEffect, useRef } from 'react';
+import { Video } from '../types';
+import { NextPage } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+import { BsPlay, BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go'
 
 interface IProps {
     post: Video
 }
 
 const VideoCard: NextPage<IProps> = ({post}) => {
-    console.log(post.caption)
+
 
   return (
-    <div>VideoCard</div>
+    <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
+        <div>
+            <div className='flex  gap-3 p-2 cursor-pointer font-semibold rounded'>
+                {/* USER IMAGE */}
+                <div className='md:w-16 md:h-16 w-10 h-10'>
+                    <Link href="/">
+                        <>
+                            <Image 
+                                width={62}
+                                height={62}
+                                className='rounded-full'
+                                src={post.postedBy.image}
+                                alt='profile photo'
+                                layout='responsive'
+                            />
+                        </>
+                    </Link>
+                </div>
+                {/* USER NAME */}
+                <div>
+                    <Link href="/">
+                        <div className='flex items-center gap-2'>
+                            <p className='flex gap-2 items-center md:text-md font-bold text-primary'>
+                                {post.postedBy.userName}
+                                {""}
+                                <GoVerified className='text-blue-400 text-md' />
+                            </p>
+                            <p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>
+                                {post.postedBy.userName}
+                            </p>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </div>
+        {/* VIDEO POST */}
+    </div>
   )
 }
 
--- next.config.js
+++ next.config.js
@@ -6,7 +6,9 @@ const nextConfig = {
         'lh3.googleusercontent.com',
         'cdn.pixabay.com',
         'p16-amd-va.tiktokcdn.com',
-        'image.shutterstock.com'
+        'image.shutterstock.com',
+        'avatars.githubusercontent.com',
+        'scontent-lax3-1.xx.fbcdn.net'
       ],
     },
   };
