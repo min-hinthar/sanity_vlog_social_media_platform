a77f47f0a3a3454ee0b94a8985c98db82669da93
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-04 00:51:59 -0800

    GoogleLogin npm installed @react-oauth/google and imported into Navbar with onSuccess and onError requirements

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { GoogleLogin, GoogleLogout } from '@react-oauth/google';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
@@ -10,6 +10,9 @@ import Logo from '../utils/tiktik-logo.png';
 import Logo2 from '../utils/Logo2.png';
 
 const Navbar = () => {
+
+  const user = false;
+
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
       <Link href='/'>
@@ -28,6 +31,24 @@ const Navbar = () => {
           />
         </div>
       </Link>
+
+      <div>
+        SEARCH
+      </div>
+
+      <div>
+        {user ? (
+          <div>
+            Logged In
+          </div>
+        ) : (
+          <GoogleLogin 
+            onSuccess={(response) => console.log(response) }
+            onError={() => console.log('Error')}
+          />
+        )}
+      </div>
+
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import { useState, useEffect } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
@@ -16,6 +17,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   if(isSSR) return null;
 
   return (
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
     <div>
       <Navbar />
       <div className='flex gap-6 md:gap-20'>
@@ -27,6 +29,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         </div>
       </div>
     </div>
+    </GoogleOAuthProvider>
   )
 };
 
