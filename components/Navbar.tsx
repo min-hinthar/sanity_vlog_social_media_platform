501cf32ccaf7fbe7660067a2ac93e1650bd9cdc0
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-04 20:08:35 -0800

    Zustand imported to create(persist(authstore, userProfile, addUser) and invoked in Navbar for Google Auth signing in

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,10 +8,12 @@ import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 import Logo from '../utils/tiktik-logo.png';
 import Logo2 from '../utils/Logo2.png';
+import { createOrGetUser } from '../utils';
+import useAuthStore from '../store/authStore';
 
 const Navbar = () => {
 
-  const user = false;
+  const { userProfile, addUser } = useAuthStore();
 
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
@@ -37,13 +39,13 @@ const Navbar = () => {
       </div>
 
       <div>
-        {user ? (
+        {userProfile ? (
           <div>
-            Logged In
+            {userProfile?.userName}
           </div>
         ) : (
           <GoogleLogin 
-            onSuccess={(response) => console.log(response) }
+            onSuccess={(response) => createOrGetUser(response, addUser)}
             onError={() => console.log('Error')}
           />
         )}
