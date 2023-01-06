1d0e37b4a7f9273892dd566b89aadd5f9674a3b7
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-05 21:56:02 -0800

    addComment button styling updated

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -46,7 +46,7 @@ const Comments = ({ comment, setComment, addComment, comments, isPostingComment 
               placeholder="Add a comment..."
               className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg'
             />
-            <button className='text-md text-gray-400' onClick={addComment}>
+            <button className='text-md text-gray-400 border-4 p-2 rounded-full hover:bg-[gray] hover:text-white' onClick={addComment}>
               {isPostingComment ? 'Commenting...' : 'Comment'}
             </button>
           </form>
