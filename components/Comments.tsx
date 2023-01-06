f12fccd2b022f40033ab752f7b0cb85522feb0ba
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-05 21:50:54 -0800

    Interface IProps, IComment for comment, setComment, addComment, comments, isPostingComment updated, addComment handle function updated in [id].tsx details, api 'PUT' comment handler updated

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Dispatch, SetStateAction} from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { GoVerified } from 'react-icons/go';
@@ -6,16 +6,29 @@ import { GoVerified } from 'react-icons/go';
 import useAuthStore from '../store/authStore';
 import NoResults from './NoResults';
 
+interface IProps {
+  isPostingComment: Boolean,
+  comment: string,
+  // IMPORTANT TO DISPATCH SETACTION FORMEVENT!
+  setComment: Dispatch<SetStateAction<string>>;
+  addComment: (e: React.FormEvent) => void;
+  comments: IComment[];
+}
 
-const Comments = () => {
-  const userProfile = useAuthStore();
-  const comments = [];
-  const isPostingComment = false;
+interface IComment {
+  comment: string,
+  length?: number,
+  _key: string,
+  postedBy: { _ref: string; _id: string};
+}
+
+const Comments = ({ comment, setComment, addComment, comments, isPostingComment }: IProps) => {
+  const {userProfile} : any = useAuthStore();
 
   return (
     <div className='border-t-2 border-gray-200 pt-4 px-10 bg-[#f8f8f8] border-b-2 lg:pb-0 pb-[100px]'>
       <div className='overflow-scroll lg:h-[475px]'>
-        {comments.length ? (
+        {comments?.length ? (
           <div>
             videos
           </div>
@@ -26,14 +39,14 @@ const Comments = () => {
 
       {userProfile && (
         <div className='absolute bottom-0 left-0 pb-2 px-2 md:px-10'>
-          <form onSubmit={() => {}} className='flex gap-4'>
+          <form onSubmit={addComment} className='flex gap-4'>
             <input 
-              value=""
-              onChange={() => {}}
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
               placeholder="Add a comment..."
               className='bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg'
             />
-            <button className='text-md text-gray-400' onClick={() => {}}>
+            <button className='text-md text-gray-400' onClick={addComment}>
               {isPostingComment ? 'Commenting...' : 'Comment'}
             </button>
           </form>
--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -24,6 +24,8 @@ const Detail = ({ postDetails }: IProps) => {
     const [isVideoMuted, setIsVideoMuted] = useState(false);
     const router = useRouter();
     const {userProfile} : any = useAuthStore();
+    const [comment, setComment] = useState('');
+    const [isPostingComment, setIsPostingComment] = useState(false);
 
     const onVideoClick = () => {
         if(playing) {
@@ -50,6 +52,23 @@ const Detail = ({ postDetails }: IProps) => {
             })
 
             setPost({ ...post, likes: data.likes });
+        }
+    }
+
+    const addComment = async (e) => {
+        e.preventDefault();
+
+        if(userProfile && comment) {
+            setIsPostingComment(true);
+
+            const { data } = await axios.put(`${BASE_URL}/api/post/${post._id}`, {
+                userId: userProfile._id,
+                comment
+            });
+
+            setPost({ ...post, comments: data.comments });
+            setComment('');
+            setIsPostingComment(false);
         }
     }
 
@@ -149,7 +168,11 @@ const Detail = ({ postDetails }: IProps) => {
                     )}
                 </div>
                     <Comments 
-                
+                        comment={comment}
+                        setComment={setComment}
+                        addComment={addComment}
+                        comments={post.comments}
+                        isPostingComment={isPostingComment}
                 />
             </div>
         </div>
