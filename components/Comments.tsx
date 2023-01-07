da54fbdc38c7454d7793a017203b7d4213ac0882
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-06 16:18:48 -0800

    Comment section fetchAllUser and allUsers.map((user:IUser)) render user.image, user.name and item.comment

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -5,6 +5,7 @@ import { GoVerified } from 'react-icons/go';
 
 import useAuthStore from '../store/authStore';
 import NoResults from './NoResults';
+import { IUser } from '../types';
 
 interface IProps {
   isPostingComment: Boolean,
@@ -23,15 +24,50 @@ interface IComment {
 }
 
 const Comments = ({ comment, setComment, addComment, comments, isPostingComment }: IProps) => {
-  const {userProfile} : any = useAuthStore();
+  const {userProfile, allUsers} : any = useAuthStore();
 
   return (
     <div className='border-t-2 border-gray-200 pt-4 px-10 bg-[#f8f8f8] border-b-2 lg:pb-0 pb-[100px]'>
       <div className='overflow-scroll lg:h-[475px]'>
         {comments?.length ? (
-          <div>
-            videos
-          </div>
+          comments.map((item, idx) => (
+            <>
+              {allUsers.map((user: IUser) => (
+                user._id === ( item.postedBy._id || item.postedBy._ref ) && (
+                  <div className='p-2 items-center' key={idx}>
+                    <Link href={`/profile/${user._id}`}>
+                      <div className='flex items-start gap-3'>
+                        <div className='w-8 h-8'>
+                          <Image 
+                            src={user.image}
+                            width={34}
+                            height={34}
+                            className='rounded-full'
+                            alt='User Profile'
+                            layout='responsive'
+                          />
+                        </div>
+                        <div className='hidden xl:block'>
+                          <p className='flex gap-1 items-center text-md font-bold text-primary lowercase'>
+                            {user.userName.replaceAll(' ', '')}
+                            <GoVerified className='text-blue-400' />
+                          </p>
+                          <p className='capitalize text-gray-400 text-xs'>
+                            {user.userName}
+                          </p>
+                        </div>
+                      </div>
+                    </Link>
+                    <div>
+                      <p>
+                        {item.comment}
+                      </p>
+                    </div>
+                  </div>
+                )
+              ))}
+            </>
+          ))
         ) : (
           <NoResults text='No comments yet!' />
         )}
