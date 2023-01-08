f5fa487ad9d7df5474348efb549ba269baccca7c
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-07 18:26:52 -0800

    SuggestedAccounts rendering error fixed by declaring users = allUsers to sort and slice

--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -14,13 +14,17 @@ const SuggestedAccounts = () => {
     fetchAllUsers();
   }, [fetchAllUsers]);
 
+  const users = allUsers
+  .sort(() => 0.5 - Math.random())
+  .slice(0, allUsers.length);
+
   return (
     <div className='xl:border-b-2 border-gray-200 pb-4'>
       <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'>
         Suggested Accounts
       </p>
       <div>
-        {allUsers.slice(0, 6).map((user: IUser) => (
+        {users?.slice(0, 6).map((user: IUser) => (
           <Link href={`/profile/${user._id}`} key={user._id}>
             <div className='flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded'>
               <div className='w-8 h-8'>
