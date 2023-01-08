484d919769d914f31be3c70b149c38618e6cc1f8
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-07 19:20:56 -0800

    Sidebar updated to fix rendering of SuggestedAccounts fetchAllUsers and allUsers using useAuthStore handle

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,18 +2,20 @@ import React, { useState } from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import GoogleLogin from 'react-google-login';
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
 import { ImCancelCircle } from 'react-icons/im'
 import Discover from './Discover';
 import SuggestedAccounts from './SuggestedAccounts';
 import Footer from './Footer';
+import useAuthStore from '../store/authStore';
 
-const Sidebar = () => {
+const Sidebar: NextPage = () => {
   const [showSidebar, setShowSidebar] = useState(true);
 
-  const userProfile = false;
+  const { pathname } = useRouter();
+  const { fetchAllUsers, allUsers }: any = useAuthStore();
 
+  const activeLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997] rounded';
   const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-center cursor-pointer font-semibold text-[#f51997] rounded'
 
   return (
@@ -28,18 +30,21 @@ const Sidebar = () => {
         <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3'>
           <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
             <Link href="/">
-              <div className={normalLink}>
+              <div className={pathname === '/' ? activeLink : normalLink}>
                 <p className='text-2xl'>
                   <AiFillHome />
                 </p>
-                <span className='text-xl hidden xl:block'>
+                <span className='capitalize text-xl hidden xl:block'>
                   Home
                 </span>
               </div>
             </Link>
           </div>
           <Discover />
-          <SuggestedAccounts />
+          <SuggestedAccounts 
+            fetchAllUsers={fetchAllUsers}
+            allUsers={allUsers}
+          />
           <Footer />
         </div>
       )}
